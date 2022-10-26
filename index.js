0074e3beaa62b591d55542bd11688794351b2ea3
Resma Islam Upoma <upoma.cdsr@gmail.com>
2022-10-26 13:32:34 +0600

    All Data linked with index.js file

--- index.js
+++ index.js
@@ -2,10 +2,29 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+const categories = require('./data/Categories.json');
+const courses = require('./data/Courses.json')
 
 app.use(cors());
 app.get('/', (req, res) => {
-    res.send('ULearning API is Running now');
+    res.send('ULearning API is Running now..');
+});
+
+app.get('/courses-categories', (req, res) => {
+    res.send(categories);
+});
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const courseDetails = courses.filter(course => course.id === id);
+    res.send(courseDetails);
+});
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const courseDetails = courses.find(course => course.id === id);
+    res.send(courseDetails);
 });
 
 app.listen(port, () => {
